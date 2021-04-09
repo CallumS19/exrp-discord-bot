92b29a736e87bc318313acb64328bfa6e9f6c54e
Callum Spiers <callumspiers01@gmail.com>
2021-04-09 07:21:49 +0100

    Removed a bunch of commands

--- index.js
+++ index.js
@@ -10,18 +10,4 @@ client.on('ready', () => {
     console.log("EXRP Discord Bot is Online!")
 })
 
-client.on('message', msg => {
-    if (msg.content === "hello") {
-        msg.reply("Hi there")
-    } else if (msg.content === "u good bro") {
-        msg.channel.send("nah")
-    } else if (msg.content === "rtn is epic") {
-        msg.react("❤️")
-    }
-})
-
-client.on("messageDelete", msg => {
-    msg.reply("stop deleting messages!!")
-})
-
 client.login(process.env.BOT_TOKEN)
