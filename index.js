96f30c4916ad2b3e105c6a1fbf920ab4aaa26279
Callum Spiers <callumspiers01@gmail.com>
2021-04-09 07:52:43 +0100

    Fixed Issue with bot not appearing online. Was just a spelling mistake

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./command/${file}`);
+    const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
@@ -28,7 +28,7 @@ client.on('message', message => {
 	const command = args.shift().toLowerCase();
 
 	if (command === 'ping') {
-		message.channel.send('Pong.');
+		client.commands.get('ping').execute(message, args);
 	} else if (command === 'beep') {
 		message.channel.send('Boop.');
 	}
